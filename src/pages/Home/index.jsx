891f6ac13e194f10b0ca5f031cacb19fc0ff5209
Iago <iagoddc@gmail.com>
2026-02-26 16:39:09 -0300

    Adicionando Css

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,47 +1,53 @@
-import './style.css'
-import Trash from '../../assets/trash.svg'
+import "./style.css";
+import Trash from "../../assets/trash.svg";
 
 function Home() {
-
-const users = [{
-  id:'121313214',
-  name:'Iago',
-  age: 32,
-  email:'iago@iago.com'
-},
-{
-  id:'121313212',
-  name:'Aline',
-  age: 27,
-  email:'aline@aline.com'
-},
-]
+  const users = [
+    {
+      id: "121313214",
+      name: "Iago",
+      age: 32,
+      email: "iago@iago.com",
+    },
+    {
+      id: "121313212",
+      name: "Aline",
+      age: 27,
+      email: "aline@aline.com",
+    },
+  ];
 
   return (
-    <div className='container'>
+    <div className="container">
       <form action="">
         <h1>Cadastro de Usuários</h1>
-        <input name='nome' type='text'/>
-        <input name='idade' type='number'/>
-        <input name='email' type='email'/>
-        <button type='button'>Cadastrar</button>
+        <input placeholder="Nome" name="nome" type="text" />
+        <input placeholder="Idade" name="idade" type="number" />
+        <input placeholder="E-mail" name="email" type="email" />
+        <button type="button">Cadastrar</button>
       </form>
-      
-      
-      <div>
-        <div>
-          <p>Nome:</p>
-          <p>Idade:</p>
-          <p>E-mail:</p>
-          <button>
-            <img src={Trash} alt="lixeira"/>
-          </button>
-        </div>
-      </div>
 
+      {users.map((user) => (
+        <div key={user.id} className="card">
+          <div>
+            <p>
+              Nome: <span>{user.name}</span>
+            </p>
+            <p>
+              Idade: <span>{user.age}</span>
+            </p>
+            <p>
+              E-mail: <span>{user.email}</span>
+            </p>
+          </div>
+            <button>
+              <img src={Trash} alt="lixeira" />
+            </button>
+        </div>
+      ))}
+      
     </div>
-  )
-  
+  );
 }
 
-export default Home
+export default Home;
